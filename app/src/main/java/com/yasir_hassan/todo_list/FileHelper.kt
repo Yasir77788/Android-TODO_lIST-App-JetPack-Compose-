7c5398b382140a9106ffba5f56fc949574b2e89b
Yasir_Hassan <yasir.hassan7722@gmail.com>
2024-10-29 01:50:33 -0500

    add FileHelper for writing and reading data

---
+++ app/src/main/java/com/yasir_hassan/todo_list/FileHelper.kt
@@ -0,0 +1,46 @@
+package com.yasir_hassan.todo_list
+
+import android.content.Context
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+
+// file of list to be saved on the device memory
+const val FILE_NAME = "todolist.dat" // or txt
+
+// SnapshotStateList<> is used for saving and retrieving list items
+// SnapshotStateList<> is an implementation of mutableList than can be oberved in
+// Snapshot, which is the result created by mutableStateListOf.
+// this class implements the same semantic as arrayList.
+// using FileOutputStream class for saving the data
+fun wrightData(items: SnapshotStateList<String>, context: Context){
+    // write data to file
+    val fos = context.openFileOutput(FILE_NAME, Context.MODE_PRIVATE) // reached within the application
+    val oos = ObjectOutputStream(fos) //
+    // convert the snapshotStateList to an arrayList before saving
+    val itemList = ArrayList<String>() // create the arrayList
+    itemList.addAll(items) // convert the snapshotStateList to ArrayList
+    // write the arrayList to the file
+    oos.writeObject(itemList)
+
+    oos.close() // close the file in the device memory
+
+}
+
+// function to read data which return a snapshotStateList of String
+// Using FileInputStream class for reading the data
+fun readData(context: Context): SnapshotStateList<String>{
+    // transfer the data to an array list
+    var itemList: ArrayList<String>
+    val fis = context.openFileInput(FILE_NAME)
+    val ois = ObjectInputStream(fis)
+    itemList = ois.readObject() as ArrayList<String>  // cast data from object to string
+
+    // Conver the itemLIst to SnapshotStateList to be returned by the function
+    val items = SnapshotStateList<String>() // create SSSL
+    // convert the arrayList to SSSL
+    items.addAll(itemList)
+    return items
+}
+
+
--- app/src/main/java/com/yasir_hassan/todo_list/MainActivity.kt
+++ app/src/main/java/com/yasir_hassan/todo_list/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,25 +16,36 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontVariation.weight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -63,6 +75,11 @@ fun MainPage(modifier: Modifier = Modifier) {
     // variable to observe the value of the textField when the value changes
     val todoName = remember {
         mutableStateOf("")
+    }
+
+    // create a list for the item function
+    val itemList = remember {
+        mutableStateListOf("Learn Kotlin", "Lean Compose")
     }
     // The main layout is a column
     Column(modifier = Modifier.fillMaxSize()) {
@@ -114,6 +131,66 @@ fun MainPage(modifier: Modifier = Modifier) {
                 Text(text = "Add", fontSize = 20.sp)
             }
         }
+
+        // create Lazy column for the list items
+        LazyColumn() {
+
+            // add item function
+            items(
+                count = itemList.size,
+                itemContent = { index -> // index is the item index
+                    val item = itemList[index]
+                    // create a card for each item
+                    Card(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(1.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            contentColor = Color.White
+                        ),
+                        // remove color of the card
+                        shape = RoundedCornerShape(0.dp)
+                    ) {
+                        // add row to hold text for the icon, to cover the entire card horizontally
+                        Row(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(10.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            // add a text component in the row
+                            Text(
+                                text = item, color = Color.White, fontSize = 18.sp, maxLines = 2,
+                                overflow = TextOverflow.Ellipsis,
+                                modifier = Modifier.width(300.dp)
+                            )
+                            // create a row for the icon
+                            Row() {
+                                // add an icon button
+                                IconButton(onClick = {}) {
+                                    Icon(
+                                        Icons.Filled.Edit, contentDescription = "edit",
+                                        tint = Color.White
+                                    )
+                                }
+                                IconButton(onClick = {}) {
+                                    Icon(
+                                        Icons.Filled.Delete, contentDescription = "delete",
+                                        tint = Color.White
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            )
+
+
+        }
+
     }
 }
+
 
