71297bb80936318912e348dd17c5d6328f89c9d9
Yasir_Hassan <yasir.hassan7722@gmail.com>
2024-10-29 00:11:57 -0500

    create MainPage and column layout

--- app/src/main/java/com/yasir_hassan/todo_list/MainActivity.kt
+++ app/src/main/java/com/yasir_hassan/todo_list/MainActivity.kt
@@ -4,13 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontVariation.weight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.yasir_hassan.todo_list.ui.theme.ToDo_ListTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +46,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ToDo_ListTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainPage(
+                        //name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +56,64 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainPage(modifier: Modifier = Modifier) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ToDo_ListTheme {
-        Greeting("Android")
+    // variable to observe the value of the textField when the value changes
+    val todoName = remember {
+        mutableStateOf("")
+    }
+    // The main layout is a column
+    Column(modifier = Modifier.fillMaxSize()) {
+        // add a top row
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // add a texfield for the top tow
+            TextField(
+                value = todoName.value,
+                onValueChange = {
+                    todoName.value = it
+                },
+                label = { Text(text = "Enter Todo") },
+                colors = TextFieldDefaults.textFieldColors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedLabelColor = Color.Green,
+                    unfocusedLabelColor = Color.White,
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    focusedTextColor = Color.White,
+                    cursorColor = Color.White
+                ),
+                shape = RoundedCornerShape(5.dp),
+                modifier = Modifier
+                    .border(1.dp, Color.Black, RoundedCornerShape(5.dp))
+                    .weight(7F) // 3F, parts, for the button weight
+                    .height(60.dp), // the height of the text field
+                textStyle = TextStyle(textAlign = TextAlign.Center)
+            )
+
+            Spacer(modifier = Modifier.width(5.dp))
+
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .weight(3F)
+                    .height(60.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = R.color.green),
+                    contentColor = Color.White
+                ),
+                shape = RoundedCornerShape(5.dp),
+                border = BorderStroke(1.dp, Color.Black)
+            ) {
+                Text(text = "Add", fontSize = 20.sp)
+            }
+        }
     }
 }
+
